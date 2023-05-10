9808bf888b9df978ccde633dce968e5a25c2aa9a
Wattima <darwattima@gmail.com>
2023-05-10 23:05:04 +0300

    conditionals for a single round

--- index.js
+++ index.js
@@ -11,12 +11,42 @@
 
 // declaring array with items
 
-arr = ["rock", "paper", "scissors"];
+let arr = ["rock", "paper", "scissors"];
 
 // computer random selection function
 function getComputerChoice() {
     // select random index from array
-    randomIndex = Math.floor(Math.random() * arr.length);
-    console.log(randomIndex);
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex]
 }
+let computerChoice =  getComputerChoice();
+console.log(computerChoice)
+
+// Take player's input
+
+let playerChoice = prompt("Enter your choice: ").toLowerCase()
+console.log(playerChoice)
+
+function singleRound(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+        console.log("Tie")
+    } else if (computerChoice === "rock" && playerChoice === "paper") {
+        console.log("You Win! Paper beats Rock")
+    } else if (computerChoice === "paper" && playerChoice === "rock") {
+        console.log("You Lose! Paper beats Rock")
+    } else if (computerChoice === "rock" && playerChoice === "scissors") {
+        console.log('You Lose! Rock beats scissors')
+    } else if (computerChoice === "scissors" && playerChoice === "rock") {
+        console.log('You Win! Rock beats scissors')
+    } else if (computerChoice === "paper" && playerChoice === "scissors") {
+        console.log('You Win! Scissors beat Paper')
+    } else if (computerChoice === "scissors" && playerChoice === "paper") {
+        console.log('You Lose! Scissors beat Paper')
+    } else {
+        console.log("Invalid choice")
+    }
+
+}
+
+singleRound(computerChoice, playerChoice)
 
